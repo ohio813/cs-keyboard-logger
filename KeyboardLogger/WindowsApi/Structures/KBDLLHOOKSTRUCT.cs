27e62f67aa750a8adbda54fb1c61a17f2a743984
Subhajit Sahu <wolfram77@gmail.com>
2015-02-12 08:17:43 +0530

    msllhookstruct added

---
+++ KeyboardLogger/WindowsApi/Structures/KBDLLHOOKSTRUCT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace KeyboardLogger.WindowsApi
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class KBDLLHOOKSTRUCT
+    {
+        public uint vkCode;
+        public uint scanCode;
+        public KBDLLHOOKSTRUCTFlags flags;
+        public uint time;
+        public UIntPtr dwExtraInfo;
+    }
+
+    [Flags]
+    public enum KBDLLHOOKSTRUCTFlags : uint
+    {
+        LLKHF_EXTENDED = 0x01,
+        LLKHF_INJECTED = 0x10,
+        LLKHF_ALTDOWN = 0x20,
+        LLKHF_UP = 0x80,
+    }
+}
---
+++ KeyboardLogger/WindowsApi/Structures/MSLLHOOKSTRUCT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace KeyboardLogger.WindowsApi
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public int mouseData; // be careful, this must be ints, not uints (was wrong before I changed it...). regards, cmew.
+        public int flags;
+        public int time;
+        public UIntPtr dwExtraInfo;
+    }
+}
