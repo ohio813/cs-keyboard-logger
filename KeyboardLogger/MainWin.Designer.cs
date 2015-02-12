07266a10c956baea8deb489bd2775d7cdae8a00f
Subhajit Sahu <wolfram77@gmail.com>
2015-02-12 08:33:14 +0530

    log area added

--- KeyboardLogger/MainWin.Designer.cs
+++ KeyboardLogger/MainWin.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button_Log = new System.Windows.Forms.Button();
+            this.textBox_Log = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button_Log
+            // 
+            this.button_Log.Location = new System.Drawing.Point(109, 12);
+            this.button_Log.Name = "button_Log";
+            this.button_Log.Size = new System.Drawing.Size(75, 23);
+            this.button_Log.TabIndex = 0;
+            this.button_Log.Text = "Log";
+            this.button_Log.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.Location = new System.Drawing.Point(12, 41);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.Size = new System.Drawing.Size(260, 208);
+            this.textBox_Log.TabIndex = 1;
             // 
             // MainWin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox_Log);
+            this.Controls.Add(this.button_Log);
             this.Name = "MainWin";
             this.Text = "Keyboard Logger";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_Log;
+        private System.Windows.Forms.TextBox textBox_Log;
     }
 }
 
