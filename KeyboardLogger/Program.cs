53078b7a6b12ec56e1cbe5361aab9e6c87ffb1cf
Subhajit Sahu <wolfram77@gmail.com>
2015-02-12 22:04:42 +0530

    keyboard-logger v1

--- KeyboardLogger/Program.cs
+++ KeyboardLogger/Program.cs
@@ -14,6 +14,7 @@ namespace KeyboardLogger
         [STAThread]
         static void Main()
         {
+            // time to make things invisible
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWin());
