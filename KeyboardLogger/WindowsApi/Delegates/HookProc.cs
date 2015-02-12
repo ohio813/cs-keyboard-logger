f3a3ad2a6940c9acd609d3a770127fd433bd645c
Subhajit Sahu <wolfram77@gmail.com>
2015-02-12 08:23:00 +0530

    kbdhook and mouse hook added

--- KeyboardLogger/WindowsApi/Delegates/HookProc.cs
+++ KeyboardLogger/WindowsApi/Delegates/HookProc.cs
@@ -8,5 +8,6 @@ using System.Runtime.InteropServices;
 namespace KeyboardLogger.WindowsApi
 {
     public delegate IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam);
-    public delegate IntPtr KbdHookProc(int code, WM wParam, [In]KBDLLHOOKSTRUCT lParam);
+    public delegate IntPtr LowLevelKeyboardProc(int code, WM wParam, [In]KBDLLHOOKSTRUCT lParam);
+    public delegate IntPtr LowLevelMouseProc(int code, WM wParam, [In]MSLLHOOKSTRUCT lParam);
 }
