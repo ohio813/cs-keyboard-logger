684c90bdd365d2173e00f7c610074e9f5cef320f
Subhajit Sahu <wolfram77@gmail.com>
2015-02-12 08:11:55 +0530

    hookproc added

---
+++ KeyboardLogger/WindowsApi/Delegates/HookProc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace KeyboardLogger.WindowsApi
+{
+    public delegate IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam);
+    public delegate IntPtr KbdHookProc(int code, WM wParam, [In]KBDLLHOOKSTRUCT lParam);
+}
